156b42c3750f4b823ce9ad9cc42dc33c333c9ab2
arialblack14 <tsirasalexis@yahoo.gr>
2014-02-12 00:11:23 +0200

    Added a full title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,10 +8,15 @@ describe "StaticPages" do
       expect(page).to have_content('Voting App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the  base title 'Voting App'" do
      
       visit '/static_pages/home'
-      expect(page).to have_title('Home')
+      expect(page).to have_title('Voting App')
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
@@ -40,6 +45,20 @@ describe "StaticPages" do
      
       visit '/static_pages/about'
       expect(page).to have_title('About')
+    end
+	end
+
+	describe 'Contact page' do
+  	it "should have the content 'Contact us'" do
+
+  		visit '/static_pages/contact'
+  		expect(page).to have_content('Contact us')
+  	end
+
+  	it "should have the title 'Contact'" do
+     
+      visit '/static_pages/contact'
+      expect(page).to have_title('Contact')
     end
 	end
 end
