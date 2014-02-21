c0a6023ae0662c7b5460be9ff211880aa318a6d5
arialblack14 <tsirasalexis@yahoo.gr>
2014-02-21 23:41:07 +0200

    Finish layout and routes.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,64 +1,35 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  
+  subject { page }
+
   describe "Home page" do
-    it "should have the content 'Voting App'" do
-     
-      visit '/static_pages/home'
-      expect(page).to have_content('Voting App')
-    end
+    before { visit root_path } 
 
-    it "should have the  base title 'Voting App'" do
-     
-      visit '/static_pages/home'
-      expect(page).to have_title('Voting App')
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+    it { should have_content('Voting App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
-    it "should have the content 'Need some help?'" do
-     
-      visit '/static_pages/help'
-      expect(page).to have_content('Need some help?')
-    end
+    before { visit help_path }
 
-    it "should have the title 'Help'" do
-     
-      visit '/static_pages/help'
-      expect(page).to have_title('Help')
-    end
+    it { should have_content('Need some help?') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe 'About page' do
-  	it "should have the content 'About us'" do
+    before { visit about_path }
 
-  		visit '/static_pages/about'
-  		expect(page).to have_content('About us')
-  	end
-
-  	it "should have the title 'About'" do
-     
-      visit '/static_pages/about'
-      expect(page).to have_title('About')
-    end
+  	it { should have_content('About us') }
+  	it { should have_title(full_title('About')) }
 	end
 
 	describe 'Contact page' do
-  	it "should have the content 'Contact us'" do
+    before { visit contact_path }
 
-  		visit '/static_pages/contact'
-  		expect(page).to have_content('Contact us')
-  	end
-
-  	it "should have the title 'Contact'" do
-     
-      visit '/static_pages/contact'
-      expect(page).to have_title('Contact')
-    end
+  	it { should have_content('Contact') }
+  	it { should have_title(full_title('Contact')) }
 	end
 end
